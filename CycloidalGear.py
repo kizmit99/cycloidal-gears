6b51f8973c811a49956855424d3ff7e4c7d09316
Sneezy\admin <admin@sneezy.com>
2021-02-15 20:13:53 -0500

    modified to support eccentricity and output drive holes

--- CycloidalGear.py
+++ CycloidalGear.py
@@ -1,5 +1,6 @@
 #Author-Frances O'Leary January 2021
 #Description-Fusion script with a GUI to generate 3 bodies: cycloidal disk, base plate, and cam based on input parameters.
+#ModifiedBy-kizmit99 February 2021 to generate from different parameters
 
 import adsk.core, adsk.fusion, traceback, math
 
@@ -7,8 +8,13 @@ _app = None
 _ui  = None
 
 # command inputs
-pinDiameter = adsk.core.ValueCommandInput.cast(None)
-pinNumber = adsk.core.ValueCommandInput.cast(None)
+fixedPinRingDiameter = adsk.core.ValueCommandInput.cast(None)
+fixedPinDiameter = adsk.core.ValueCommandInput.cast(None)
+fixedPinNumber = adsk.core.ValueCommandInput.cast(None)
+eccentricity = adsk.core.ValueCommandInput.cast(None)
+outputPinRingDiameter = adsk.core.ValueCommandInput.cast(None)
+outputPinDiameter = adsk.core.ValueCommandInput.cast(None)
+outputPinNumber = adsk.core.ValueCommandInput.cast(None)
 camR = adsk.core.ValueCommandInput.cast(None)
 centerPinR = adsk.core.ValueCommandInput.cast(None)
 camTolerance = adsk.core.ValueCommandInput.cast(None)
@@ -44,7 +50,7 @@ class MyCommandDestroyHandler(adsk.core.CommandEventHandler):
             _ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
 
 
-# event handler that reacts when the command definitio is executed which
+# event handler that reacts when the command definition is executed which
 # results in the command being created and this event being fired.
 class MyCommandCreatedHandler(adsk.core.CommandCreatedEventHandler):
     def __init__(self):
@@ -59,14 +65,19 @@ class MyCommandCreatedHandler(adsk.core.CommandCreatedEventHandler):
             cmd.inputChanged.add(onInputChanged)
             _handlers.append(onInputChanged)    
             inputs = cmd.commandInputs
-            global pinDiameter, pinNumber, camR, centerPinR, camTolerance, baseHeight, pinHeight, camHeight, diskHeight
+            global fixedPinRingDiameter, fixedPinDiameter, fixedPinNumber, eccentricity, outputPinRingDiameter, outputPinDiameter, outputPinNumber, camR, centerPinR, camTolerance, baseHeight, pinHeight, camHeight, diskHeight
 
             # add inputs
             inputs.addImageCommandInput('image', '', "resources/diagram.png")
             message = '<div align="center">View the documentation <a href="https://github.com/olearyf/cycloidal-gears">here.</a></div>'
             inputs.addTextBoxCommandInput('fullWidth_textBox', '', message, 1, True)            
-            pinDiameter = inputs.addValueInput('pinDiameter', 'Pin Diameter', 'cm', adsk.core.ValueInput.createByReal(0.5))
-            pinNumber = inputs.addValueInput('pinNumber', 'Number of Pins', '', adsk.core.ValueInput.createByReal(10))
+            fixedPinRingDiameter = inputs.addValueInput('fixedPinRingDiameter', 'Fixed Pin Ring Diameter', 'cm', adsk.core.ValueInput.createByReal(5.0))
+            fixedPinDiameter = inputs.addValueInput('fixedPinDiameter', 'Fixed Pin Diameter', 'cm', adsk.core.ValueInput.createByReal(0.5))
+            fixedPinNumber = inputs.addValueInput('fixedPinNumber', 'Number of Fixed Pins', '', adsk.core.ValueInput.createByReal(10))
+            eccentricity = inputs.addValueInput('eccentricity', 'Eccentricity', '', adsk.core.ValueInput.createByReal(0.125))
+            outputPinRingDiameter = inputs.addValueInput('outputPinRingDiameter', 'Output Pin Ring Diameter', '', adsk.core.ValueInput.createByReal(3.0))
+            outputPinDiameter = inputs.addValueInput('outputPinDiameter', 'Output Pin Diameter', '', adsk.core.ValueInput.createByReal(0.3))
+            outputPinNumber = inputs.addValueInput('outputPinNumber', 'Number of Output Pins', '', adsk.core.ValueInput.createByReal(6))
             camR = inputs.addValueInput('camR', 'Cam Radius', 'cm', adsk.core.ValueInput.createByReal(1.0))
             centerPinR = inputs.addValueInput('centerPinR', 'Center Pin Radius', 'cm', adsk.core.ValueInput.createByReal(0.15))
             camTolerance = inputs.addValueInput('camTolerance', 'Cam Tolerance', 'cm', adsk.core.ValueInput.createByReal(0.05))
@@ -88,7 +99,7 @@ def run(context):
         _ui = _app.userInterface
         cmdDef = _ui.commandDefinitions.itemById('cmdInputsCyclGear')
         if not cmdDef:
-            cmdDef = _ui.commandDefinitions.addButtonDefinition('cmdInputsCyclGear', 'Cycloidal Gear', 'Creates a cycloidal gear based on input parameters.')
+            cmdDef = _ui.commandDefinitions.addButtonDefinition('cmdInputsCyclGear', 'My Cycloidal Gear', 'Creates a cycloidal gear based on input parameters.')
         onCommandCreated = MyCommandCreatedHandler()
         cmdDef.commandCreated.add(onCommandCreated)
         _handlers.append(onCommandCreated)
@@ -103,11 +114,19 @@ class CycloidalGearCommandExecuteHandler(adsk.core.CommandEventHandler):
         super().__init__()
     def notify(self, args):
         try:
-            global pinDiameter, pinNumber, camR, centerPinR, camTolerance, baseHeight, pinHeight, camHeight, diskHeight
+            global fixedPinRingDiameter, fixedPinDiameter, fixedPinNumber, eccentricity, outputPinRingDiameter, outputPinDiameter, outputPinNumber, camR, centerPinR, camTolerance, baseHeight, pinHeight, camHeight, diskHeight
             eventArgs = adsk.core.CommandEventArgs.cast(args)
-            
-            pinDiameter = pinDiameter.value
-            pinNumber = int(pinNumber.value)
+
+            fixedPinRingDiameter = fixedPinRingDiameter.value
+            fixedPinDiameter = fixedPinDiameter.value
+            fixedPinNumber = int(fixedPinNumber.value)
+            eccentricity = eccentricity.value
+            maxE = (fixedPinRingDiameter / (2 * fixedPinNumber))
+            if (eccentricity > maxE):
+                eccentricity = maxE
+            outputPinRingDiameter = outputPinRingDiameter.value
+            outputPinDiameter = outputPinDiameter.value
+            outputPinNumber = int(outputPinNumber.value)
             camR = camR.value
             centerPinR = centerPinR.value
             camTolerance = camTolerance.value
@@ -127,16 +146,18 @@ class CycloidalGearCommandExecuteHandler(adsk.core.CommandEventHandler):
             extrudes = rootComp.features.extrudeFeatures
             sketches = rootComp.sketches
             xyPlane = rootComp.xYConstructionPlane
-            drawCycloidalBodies(combineFeatures, app, xyPlane, sketches, extrudes, pinDiameter, pinNumber, camR, centerPinR, camTolerance, baseHeight, pinHeight, camHeight, diskHeight)
+            drawCycloidalBodies(combineFeatures, app, xyPlane, sketches, extrudes, fixedPinRingDiameter, fixedPinDiameter, fixedPinNumber, eccentricity, outputPinRingDiameter, outputPinDiameter, outputPinNumber, camR, centerPinR, camTolerance, baseHeight, pinHeight, camHeight, diskHeight)
         except:
             if _ui:
                 _ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
 
 # generates the cycloidal disk, cam, and base plate with pins
-def drawCycloidalBodies(combineFeatures, app, xyPlane, sketches, extrudes, pinD, numPins, shaftR, centerpinR, tolerance, baseHeight, pinHeight, camHeight, diskHeight):
+def drawCycloidalBodies(combineFeatures, app, xyPlane, sketches, extrudes, fixedD, fixedDp, fixedN, eccentricity, outputD, outputDp, outputN, shaftR, centerpinR, tolerance, baseHeight, pinHeight, camHeight, diskHeight):
     # further calculations
-    rotorR = pinD * numPins * 2 / 3
-    eccentricity = pinD * 0.5
+    rollingD = ((fixedN - 1) / fixedN) * fixedD
+    rollingR = rollingD / 2
+    sigma = fixedD / fixedN
+    effectiveR = rollingR + (sigma / 2)
 
     # create sketches and tools/collections
     diskSketch = sketches.add(xyPlane)
@@ -155,27 +176,37 @@ def drawCycloidalBodies(combineFeatures, app, xyPlane, sketches, extrudes, pinD,
     px = 0
     py = 0
     pz = 0
-    for i in range(361):
+    for i in range(360):
         currentRad = i * math.pi / 180.0
+        alpha = currentRad * fixedN
         # parametric equations to create a spline along the outer edge
-        px = (rotorR * math.cos(currentRad)) - (pinD * math.cos((currentRad) + math.atan(math.sin((1 - numPins) * (currentRad)) / ((rotorR / eccentricity * numPins) - math.cos((1 - numPins) * (currentRad)))))) - (eccentricity * math.cos(numPins * (currentRad)))
-        py = (-rotorR * math.sin((currentRad))) + (pinD * math.sin((currentRad) + math.atan(math.sin((1 - numPins) * (currentRad)) / ((rotorR / eccentricity * numPins) - math.cos((1 - numPins) * (currentRad)))))) + (eccentricity * math.sin(numPins * (currentRad)))
-        points.add(adsk.core.Point3D.create(px, py, pz))
+        px = effectiveR * math.cos(currentRad) + (eccentricity * math.cos(alpha))
+        py = effectiveR * math.sin(currentRad) + (eccentricity * math.sin(alpha))
+        point = adsk.core.Point3D.create(px, py, pz)
+        points.add(point)
+        if (i == 0):
+            point0 = point
+    points.add(point0)
     spline = diskSketch.sketchCurves.sketchFittedSplines.add(points)
     curves = diskSketch.findConnectedCurves(diskSketch.sketchCurves.sketchFittedSplines.add(points))
     dirPoint = adsk.core.Point3D.create(0, 0, 0)
-    offsetCurves = diskSketch.offset(curves, dirPoint, pinD)
+    offsetCurves = diskSketch.offset(curves, dirPoint, (fixedDp / 2.0))
     shaftHole = diskCircles.addByCenterRadius(adsk.core.Point3D.create(0, 0, 0), shaftR)
+    for i in range(outputN):
+        angle = i * 2.0 * math.pi / outputN
+        px = (outputD / 2.0) * math.cos(angle)
+        py = (outputD / 2.0) * math.sin(angle)
+        diskCircles.addByCenterRadius(adsk.core.Point3D.create(px, py, 0), outputDp)
 
     # extrude the disk
-    prof = diskSketch.profiles.item(2)
+    prof = diskSketch.profiles.item(1)
     distance = adsk.core.ValueInput.createByReal(diskHeight)
     diskExtrude = extrudes.addSimple(prof, distance, adsk.fusion.FeatureOperations.NewBodyFeatureOperation)        
     disk = diskExtrude.bodies.item(0)
     disk.name = "cycloidal disk"
 
     # create the sketch for the base
-    baseCircle = baseCircles.addByCenterRadius(adsk.core.Point3D.create(0, 0, 0), app.measureManager.measureMinimumDistance(curves.item(0), dirPoint).value + pinD + 0.5)
+    baseCircle = baseCircles.addByCenterRadius(adsk.core.Point3D.create(0, 0, 0), (fixedD / 2.0) + (fixedDp / 2.0) + 0.5)
 
     # extrude the base
     prof = baseSketch.profiles.item(0)
@@ -193,13 +224,13 @@ def drawCycloidalBodies(combineFeatures, app, xyPlane, sketches, extrudes, pinD,
     base2 = baseExtrude2.bodies.item(0)
 
     # create the sketches for the outer pins
-    degreeInc = (360.0 / (numPins+1)) * (math.pi / 180.0)
+    degreeInc = (360.0 / fixedN) * (math.pi / 180.0)
     x = 0
-    y = app.measureManager.measureMinimumDistance(curves.item(0), dirPoint).value + pinD*0.5
+    y = fixedD / 2.0
     z = 0
     count = 2
-    for i in range(numPins+1):
-        baseCircles.addByCenterRadius(adsk.core.Point3D.create(x, y, z), pinD * 0.5)
+    for i in range(fixedN):
+        baseCircles.addByCenterRadius(adsk.core.Point3D.create(x, y, z), fixedDp * 0.5)
         newy = x*math.sin(degreeInc) + y*math.cos(degreeInc)
         newx = x *math.cos(degreeInc) - y*math.sin(degreeInc)
         x = newx
@@ -224,7 +255,7 @@ def drawCycloidalBodies(combineFeatures, app, xyPlane, sketches, extrudes, pinD,
     # create the sketch for the cam, allowing for tolerances
     shaftHole = camCircles.addByCenterRadius(adsk.core.Point3D.create(0, 0, 0), shaftR - (tolerance))
     # pinD*0.5
-    offsetPin = camCircles.addByCenterRadius(adsk.core.Point3D.create(0, pinD * 2 / 3, 0), centerpinR + tolerance) 
+    offsetPin = camCircles.addByCenterRadius(adsk.core.Point3D.create(0, eccentricity, 0), centerpinR + tolerance) 
 
     # extrude the cam
     prof = camSketch.profiles.item(0)
